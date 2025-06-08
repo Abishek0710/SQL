7f2b37b4ce9031ef0451a2960ad21c0d8324cc55
Abishek R <abishekr800@gmail.com>
2025-06-08 11:53:08 +0530

    DCL, TCL commands

--- 13_Advanced_update_and_insert.sql
+++ 13_Advanced_update_and_insert.sql
@@ -46,21 +46,73 @@ set dept_name = d.dep_name;
 select * from employee;
 
 
+# Deleting using join
+
+delete e 
+from employee e
+left join dept d
+on e.dept_id = d.dep_id
+where d.dep_name = 'HR';
+
+delete from employee where dept_id not in ( select dep_id from dept);
+
+select * from employee;
 
 
 
+# exists and not exists
+
+select * from employee e
+where exists ( select 1 from dept d  where e.dept_id = d.dep_id);
+
+select * from employee e
+where not exists ( select 1 from dept d  where e.dept_id = d.dep_id);
+
+/* exists and not exists are not used mostly 
+everything that is performed with exists clause can be done with where clause*/
+
+-- ddl - create, drop , alter
+-- dml - insert, update, delete
+-- dql - select
+-- dcl - grant, revoke
 
 
+# DCL - Data control Language
+
+# to give access to a user name with guest
+grant select , insert on namastesql.employee to guest;
+
+grant select , insert, create, update on namastesql.employee to public;
+
+# revoke access
+revoke select , insert on namastesql.employee from guest;
 
 
+create role role_sales;
+grant select , insert on namastesql.employee to role_sales;
+
+# using with grant option gives access to the users in a role to provide access
+grant select , insert on namastesql.employee to role_sales with grant option;
 
 
+# TCL - Transaction Control Language -- commit and rollback
 
 
+# we are starting a transaction with some dml operation
 
+start transaction ;
+update employee set salary = 30000 where emp_id = 1;
 
+# checking if the operation is affected
+select * from employee;
 
+# if we are not satisfied with the operation we can undo using rollback
+rollback ;
 
+start transaction ;
+update employee set salary = 30000 where emp_id = 1;
 
+# if we want the operation to be permanent we can use commit
+commit;
 
 
